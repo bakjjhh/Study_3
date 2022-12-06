16ceb11dba7d719b83ad2dea6c1c9ccfa31dc385
junghun bak <wjdgnsi79@naver.com>
2022-12-06 12:35:25 +0900

    step1

--- Study_3/ViewController.swift
+++ Study_3/ViewController.swift
@@ -8,6 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBAction func tapviewAction(_ sender: UITapGestureRecognizer) {
+        if sender.state == UITapGestureRecognizer.State.recognized {
+  //ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡTextFieldㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+           
+            let Loc = sender.location(in: sender.view)
+          
+            let myImage = #imageLiteral(resourceName: "Image")
+            
+            let textview = UITextField (frame:CGRect(x: Loc.x, y: Loc.y, width:100, height: 80))
+            
+            textview.layer.cornerRadius = 30
+            
+            textview.borderStyle = .none
+            
+            textview.background = myImage
+            
+            self.view.addSubview(textview)
+        }
+    }
+   //ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
 
     override func viewDidLoad() {
         super.viewDidLoad()
